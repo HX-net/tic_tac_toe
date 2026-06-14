967e6b3b3fdaef71b25257b2a8ca3c5f92484129
HX <hx.net1@gmail.com>
2026-06-14 20:14:31 +0330

    Finish theme, Need Game Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp()
-    ));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,24 +11,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello")),
+      backgroundColor: const Color.fromARGB(255, 11, 20, 68),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 2, 8, 49),
+        title: Text("Tic Tac Toe", style: TextStyle(color: Colors.white)),
+      ),
       body: Column(
         children: [
           Column(
             children: [
+              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Text("Player X"), Text("Player Y")],
+                children: [
+                  Text("Player X", style: TextStyle(color: Colors.white)),
+                  Text("Player Y", style: TextStyle(color: Colors.white)),
+                ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Text("0"), Text("0")],
+                children: [
+                  Text("0", style: TextStyle(color: Colors.white)),
+                  Text("0", style: TextStyle(color: Colors.white)),
+                ],
               ),
             ],
           ),
-          Table(),
+          SizedBox(height: 100),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              // columnWidths: const {0: FlexColumnWidth()},
+              border: TableBorder(
+                horizontalInside: BorderSide(color: Colors.white),
+                verticalInside: BorderSide(color: Colors.white),
+              ),
+              children: [
+                _buildSquareRow(3),
+                _buildSquareRow(3),
+                _buildSquareRow(3),
+              ],
+            ),
+          ),
+          SizedBox(height: 200),
+          Container(
+            width: 100,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(1000),
+            ),
+            child: TextButton(
+              child: Text("Start Game", style: TextStyle(color: Colors.white)),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+TableRow _buildSquareRow(int number) {
+  return TableRow(
+    children: [
+      for (int num = 1; num <= number; num++)
+        Container(
+          height: 100,
+          width: 50,
+          alignment: Alignment.center,
+          child: Text(("$num"), style: TextStyle(color: Colors.white)),
+        ),
+    ],
+  );
 }
